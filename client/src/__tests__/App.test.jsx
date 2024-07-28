746e63a4ddd8730d31824a4747e503b5ae14b88e
Jamie Wilson <howemanning@gmail.com>
2024-07-28 13:30:26 +1200

    Reinstall jest

--- client/src/__tests__/App.test.jsx
+++ client/src/__tests__/App.test.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
+import { render, screen, act } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import App from "../components/App";
 
 test("renders App component", () => {
-  render(<App />);
+  act(() => {
+    render(<App />);
+  });
   expect(screen.getByText("Jamie Wilson")).toBeInTheDocument();
 });
