56cedad38d8c341b338f2c0eb6cca01688071a18
Jamie Wilson <howemanning@gmail.com>
2024-08-02 22:48:29 +1200

    Add p to hero-section, add animation with CSS

--- client/src/components/hero-section/HeroSection.jsx
+++ client/src/components/hero-section/HeroSection.jsx
@@ -10,6 +10,13 @@ const HeroSection = () => {
       <h1 className="hero-section__title" id="hero-section__heading">
         Jamie Wilson
       </h1>
+      <p className="hero-section__description">
+        Hello! I am Jamie Wilson, a passionate and driven Software Development
+        student in my final 6 months at the Ara Institute of Canterbury. I am
+        excited to bring my skills, creativity, and enthusiasm to the tech
+        industry, and I look forward to contributing to innovative and impactful
+        projects.
+      </p>
       <SignatureJW />
     </section>
   )
