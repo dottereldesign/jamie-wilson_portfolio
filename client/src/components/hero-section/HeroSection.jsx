1767ab0d4abb2c9ce3538801caa8974e09773979
Jamie Wilson <howemanning@gmail.com>
2024-08-02 23:13:37 +1200

    Test

--- client/src/components/hero-section/HeroSection.jsx
+++ client/src/components/hero-section/HeroSection.jsx
@@ -11,11 +11,8 @@ const HeroSection = () => {
         Jamie Wilson
       </h1>
       <p className="hero-section__description">
-        Hello! I am Jamie Wilson, a passionate and driven Software Development
-        student in my final 6 months at the Ara Institute of Canterbury. I am
-        excited to bring my skills, creativity, and enthusiasm to the tech
-        industry, and I look forward to contributing to innovative and impactful
-        projects.
+        Turning caffeine into code <br />
+        and ideas into interactive experiences.
       </p>
       <SignatureJW />
     </section>
